556c3b3f3ddf756c44ae2896e1b4c2192d665e8d
pdowejr <64440746+pdowejr@users.noreply.github.com>
2021-12-16 21:16:32 -0500

    Updating setup

--- src/components/profile/ProfMenu.js
+++ src/components/profile/ProfMenu.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { MdPerson } from "react-icons/md";
+import classes from "./Profile.module.scss";
 
 const ProfMenu = (props) => {
     return (
-        <div>
+        <div className={classes.container}>
             <MdPerson />
             <h5>{props.usrName}</h5>
         </div>
