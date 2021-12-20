25a8c48abcb2d95219f0b3329af64ace3e840936
pdowejr <64440746+pdowejr@users.noreply.github.com>
2021-12-20 14:11:20 -0500

    Updating banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Logo from "../../images/KC_logo.png";
 import classes from "./Banner.module.scss";
+import Navigation from "./../navigation/Navigation"
 import { RiShoppingCartLine } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 //Dummy 
 const Banner = (props) => {
     return (
-        <header className={classes.container}>
-            <img src={Logo} alt="logo" />
-            <div className={classes.banner__cart__menu}>
-                <RiShoppingCartLine className={classes.cart} />
-                <GiHamburgerMenu className={classes.menu} />
+        <header className={classes.header}>
+            <div className={classes.header__content}>
+                <img src={Logo} alt="logo" />
+                <Navigation />
+                <div className={classes.header__cart__menu}>
+                    <RiShoppingCartLine className={classes.header__cart} />
+                    <GiHamburgerMenu className={classes.header__menu} />
+                </div>
             </div>
         </header>
     )
