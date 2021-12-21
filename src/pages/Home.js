c859ca4cf0cf3523ab5301a9c3481a617e4ce635
pdowejr <64440746+pdowejr@users.noreply.github.com>
2021-12-21 13:54:46 -0500

    Video build

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,19 @@
 import React from "react";
 import Banner from "../components/banner/Banner";
-import PostCard from "../components/cards/PostCard";
+import classes from "./Home.module.scss";
+import { Player } from "video-react";
 
 function Home() {
     return (
         <div>
             <Banner Page="Home" />
-            <main style={styles.mainContainer}>
-                <PostCard id="Paul" />
-                <PostCard id="Sam" style={styles.secContainer} />
-                <PostCard id="Tom" />
+            <main>
+                <video>
+                    <Player />
+                </video>
             </main>
         </div>
     );
 }
 export default Home
 
-const styles = {
-    mainContainer: {
-        display: "flex",
-        justifyContent: "space-between",
-        height: "100%",
-
-    },
-    secContainer: {
-        display: 'flex: 2'
-    },
-    nav: {
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh'
-    },
-    row: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "100%"
-    },
-    column: {
-        display: 'flex',
-        flexDirection: 'column',
-        flexBasis: '1',
-        width: '100%',
-        flex: '1'
-    },
-}
