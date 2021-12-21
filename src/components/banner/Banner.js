8464af9540f745a76ce5110b7b17b1f9dff5356e
pdowejr <64440746+pdowejr@users.noreply.github.com>
2021-12-20 23:42:34 -0500

    Refactoring banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../../images/KC_logo.png";
 import classes from "./Banner.module.scss";
 import Navigation from "./../navigation/Navigation";
-import ProfMenu from "./../profile/ProfMenu"
+import ProfMenu from "./../profile/ProfMenu";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -13,10 +13,14 @@ const Banner = (props) => {
             <div className={classes.header__content}>
                 <img src={Logo} alt="logo" />
                 <Navigation />
-                <div className={classes.header__cart__menu}>
-                    <RiShoppingCartLine className={classes.header__cart} />
-                    <ProfMenu usrName="Log In" className={classes.header__profile} />
-                    <GiHamburgerMenu className={classes.header__menu} />
+                <div className={classes.header__cart__profile}>
+                    <div className={classes.header__cart__menu}>
+                        <RiShoppingCartLine className={classes.header__cart} />
+                        <GiHamburgerMenu className={classes.header__menu} />
+                    </div>
+                    <div className={classes.user}>
+                        <ProfMenu className={classes.header__profMenu} />
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/profile/ProfMenu.js
+++ src/components/profile/ProfMenu.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { MdPerson } from "react-icons/md";
 import classes from "./Profile.module.scss";
+import { Link } from "react-router-dom";
 
 const ProfMenu = (props) => {
     return (
-        <div className={classes.container}>
+        <div className={classes.profile}>
             <MdPerson className={classes.profile__icon} />
-            <h5>{props.usrName}</h5>
+            <h5 className={classes.profile__user}>
+            <Link to="/login">Log In</Link>
+            </h5>
         </div>
     );
 };
